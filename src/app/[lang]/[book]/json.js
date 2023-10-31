27a2ca03153d64394ab46446d0a98dfc7074c117
dooshu <doosho@hotmail.com>
2023-10-31 21:13:18 +0800

    replaceChar

--- src/app/[lang]/[book]/json.js
+++ src/app/[lang]/[book]/json.js
@@ -123,3 +123,14 @@ module.exports = {
 //   console.log(line);
 //   rl.close()
 // });
+
+function replaceChar(origString, replaceChar, index) {
+  let firstPart = origString.substr(0, index);
+
+  let lastPart = origString.substr(index + 1);
+
+  let newString =
+      firstPart + replaceChar + lastPart;
+
+  return newString;
+}
